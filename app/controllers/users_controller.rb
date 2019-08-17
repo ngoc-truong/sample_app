e5c9897537f233704f068880772b87cfc6a2c5f9
Ngoc Truong <ngoc.truong@posteo.de>
2019-08-17 12:48:06 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user,  only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: [:destroy]
+
+  def index 
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -18,6 +26,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit 
+  end
+
+  def update 
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated" 
+      redirect_to @user
+    else 
+      render "edit"
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy 
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private 
 
     def user_params
@@ -25,6 +51,29 @@ class UsersController < ApplicationController
                                     :email, 
                                     :password, 
                                     :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in? 
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user 
+      @user = User.find(params[:id])
+      # Note to me: Why does the Users controller have access to a sessions_helper? 
+      unless current_user?(@user) 
+        flash[:danger] = "Sorry, but you cannot edit this user!"
+        redirect_to(root_url) unless current_user?(@user) 
+      end
+    end
+
+    def admin_user
+      unless current_user.admin? 
+        redirect_to(root_url)
+      end
     end
 end
 
